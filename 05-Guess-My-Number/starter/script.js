cb680d06ac8b817d68f58f33a5ada06994e08426
mastapetz <mastapetz@icloud.com>
2021-03-19 18:14:41 +0100

    commit 190321

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,5 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent); //. operater werden von links nach rechts ausgeführt
+//java script interaktion und manipulation von html element = DOM manipulation DOM Document Object Model
+// DOM sind WEB APIs und kein teil von JS
